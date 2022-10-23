a5d4f83f24d9fa18e3bf6a00eaf1d045aae62122
Anand Tharad <atharad209@gmail.com>
2022-10-23 22:21:17 +0530

    initial

---
+++ routes/users.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const router = express.Router();
+
+const catchAsync = require("../Utils/catchAsync")
+const passport = require("passport")
+
+const users = require("../controllers/user")
+const products=require("../controllers/products")
+router.route("/landingpage")
+.get(catchAsync(users.landing))
+
+router.route("/register")
+    .get(users.renderNewUserForm)
+    .post(catchAsync(users.registerNewUser))
+
+router.route("/login")
+    .get(users.renderLoginForm)
+    .post(passport.authenticate("local", { failureFlash: true, failureRedirect: "/login", keepSessionInfo: true }), catchAsync(users.loginUser))
+
+router.get('/logout', users.logoutUser)
+router.route("/cart")
+    .get(catchAsync(products.shoppingcart) )
+    .post(catchAsync(products.updateCart))
+
+
+module.exports = router;
