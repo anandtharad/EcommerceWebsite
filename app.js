ee05e4a7e6050132942e0de31d3e2ef485b96fe1
Anand Tharad <atharad209@gmail.com>
2022-10-29 22:56:12 +0530

    deploy1

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.use((err, req, res, next) => {
     if (!err.message) err.message = "Something went wrong"
     res.status(statusCode).render("error", { err })
 })
-
-app.listen(3000, () => {
-    console.log("listening to port 3000");
+const port=process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("listening to port", port);
 })
